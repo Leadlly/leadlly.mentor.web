81336f5e42ca556ee603570f166b43fb593560f7
shivang-16 <shivangyadav121@gmail.com>
2024-07-09 20:07:53 +0530

    fix error

--- src/app/student-profile/layout.tsx
+++ src/app/student-profile/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 import { Mada as FontSans } from "next/font/google";
 import "../globals.css";
-import StoreProvider from "@/app/StoreProvider";
 import { Container } from "@/components";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/shared/Navbar";
-import { getUser } from "@/actions/user_actions";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -23,23 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("font-sans antialiased", fontSans.variable)}>
-        <StoreProvider>
-        <div className="lg:hidden">
-            <Navbar/>
-            </div>
-          <div
-            className="max-w-7xl w-full mx-auto flex items-start gap-3 px-0 flex-col h-screen overflow-hidden
-          <Container
-            className="py-3 flex items-start gap-3 flex-col h-screen overflow-hidden"
-          > 
-            {/*h-screen overflow-hidden*/}
-            <main className="flex-1 h-main-height w-full">{children}</main>
-           </Container>
+          <div className="lg:hidden">
+            <Navbar />
           </div>
-        </StoreProvider>
+          <div className="max-w-7xl w-full mx-auto flex items-start gap-3 px-0 flex-col h-screen overflow-hidden">
+            <Container className="py-3 flex items-start gap-3 flex-col h-screen overflow-hidden">
+              {/* h-screen overflow-hidden */}
+              <main className="flex-1 h-main-height w-full">{children}</main>
+            </Container>
+          </div>
+
       </body>
     </html>
-       
-
   );
 }
