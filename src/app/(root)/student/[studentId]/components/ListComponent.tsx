7c2f6bcfd28b75d80ac58316b4611758cdc7aa8f
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-07-31 13:34:57 +0530

    Link Update

--- src/app/(root)/student/[studentId]/components/ListComponent.tsx
+++ src/app/(root)/student/[studentId]/components/ListComponent.tsx
@@ -4,23 +4,18 @@ import SearchBar from "@/app/(root)/(dashboard)/_components/SearchBar";
 import { useEffect } from "react";
 import { getAllStudents } from "@/actions/user_actions";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
+import Link from "next/link";
 
 const Popup: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const [students, setStudents] = useState<[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchStudents = async () => {
       try {
         const data = await getAllStudents();
         console.log(data); 
-        setStudents(data.students);
-        setLoading(false);
       } catch (err) {
         console.error((err as Error).message); 
-        setError((err as Error).message);
-        setLoading(false);
       }
     };
 
@@ -29,7 +24,7 @@ const Popup: React.FC<{ onClose: () => void }> = ({ onClose }) => {
 
   return (
     <>
-      <div className="absolute top-[50%] md:left-[15%] lg:left-[8%] transform -translate-y-1/2 w-[30%] xl:w-[415px] bg-white border border-gray-200 shadow-lg rounded-lg z-40 overflow-auto custom__scrollbar">
+      <div className="absolute text-black top-[50%] md:left-[15%] lg:left-[8%] transform -translate-y-1/2 w-[30%] xl:w-[415px] bg-white border border-gray-200 shadow-lg rounded-lg z-40 overflow-auto custom__scrollbar">
         <div className="pt-5 w-full h-[calc(100dvh-120px)] text-center">
           <div className="w-full text-left px-4 flex flex-col mb-[35px] items-center">
             <SearchBar className="rounded-[12px] text-[16px] w-[100%] bg-white" />
@@ -37,7 +32,7 @@ const Popup: React.FC<{ onClose: () => void }> = ({ onClose }) => {
           <div className="overflow-y-scroll custom__scrollbar">
             {
               students.map((profile:any) => (
-                <div className="flex items-center px-[25px] gap-6 mb-0.5 bg-white border-b-2 rounded-md min-h-fit" key={profile.id}>
+              <Link href={`/student/${profile.id}`} className="flex items-center px-[25px] gap-6 mb-0.5 bg-white border-b-2 rounded-md min-h-fit" key={profile.id}>
               <Avatar className="size-8 md:hidden">
               <AvatarImage
                 src={profile?.avatar?.url}
@@ -74,7 +69,7 @@ const Popup: React.FC<{ onClose: () => void }> = ({ onClose }) => {
                   <p className="text-red-600 text-[10px]">Tuesday</p>
                   <div className="bg-red-600 text-white rounded-full w-[15px] h-[15px] text-[8px] text-center font-semibold">2</div>
                 </div> */}
-              </div>
+              </Link>
               ))
             }
           </div>
